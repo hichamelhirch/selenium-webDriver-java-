d254ad70e836215ccc82be6a2e6366f26d6ed160
hichamelhirch <hichamelhirchgmi@gmail.com>
2024-10-11 17:27:24 +0100

    first commit

---
+++ src/main/java/com/hicham/selenium/SeleniumTestProApplication.java
@@ -0,0 +1,13 @@
+package com.hicham.selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumTestProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumTestProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hicham/selenium/SeleniumTestProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hicham.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumTestProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
